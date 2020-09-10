51d3cde1da50153a43befa6b52a0c59178313ffd
elinfi <elinfins@outlook.com>
2020-09-10 08:20:47 +0200

    Split in training and test data before scaling. Tested with MSE and R2Score

--- Project1/p1_a.py
+++ Project1/p1_a.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numba import jit
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 def FrankeFunction(x,y):
         term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
@@ -12,6 +14,7 @@ def FrankeFunction(x,y):
         term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
         return term1 + term2 + term3 + term4
 
+@jit
 def design_matrix(x, y, p):
     # make sure x and y  are 1D
     if (len(x.shape)) > 1:
@@ -52,7 +55,7 @@ def R2Score(z, ztilde):
 
 
 # Make data
-n = 5
+n = 100
 print(f"n = {n}")
 np.random.seed(101)
 x = np.sort(np.random.uniform(0, 1, n))
@@ -60,7 +63,7 @@ y = np.sort(np.random.uniform(0, 1, n))
 x, y = np.meshgrid(x, y)
 
 #Create design matrix
-p = 10
+p = 5
 print(f"p = {p}")
 X = design_matrix(x, y, p)
 
@@ -74,7 +77,7 @@ ztilde = X @ beta_OLS
 
 # Confidence interval as function of beta
 var_beta = variance_beta(beta_OLS, X, noise)
-err_beta = 1.96*np.sqrt(var_beta/len(beta_OLS))     # 95% confidence interval
+err_beta = 1.96*np.sqrt(var_beta)     # 95% confidence interval
 
 # Plot of confidence interval
 plt.errorbar(np.linspace(1, len(beta_OLS), len(beta_OLS)), beta_OLS, err_beta, fmt='.')
@@ -89,8 +92,33 @@ print(f"MSE OLS: {MSE_OLS:.3}")
 R2Score_OLS = R2Score(z, ztilde)
 print(f"R^2 Score OLS: {R2Score_OLS:.3}")
 
+# Split in training and test data
+X_train, X_test, z_train, z_test = train_test_split(X, z.reshape(-1, 1), test_size = 0.3)
+
+# X_train = X_train[:, 1:]
+# X_test = X_test[:, 1:]
+# Scale data by subtracting the mean
+scaler = StandardScaler()   # removes the mean and scales each feature/variable to unit variance
+scaler.fit(X_train)     # compute the mean and std to be used for later scaling
+X_train_scaled = scaler.transform(X_train)  # perform standardization by centering and scaling
+X_test_scaled = scaler.transform(X_test)    # fit to data, then transform it
+# X_train_scaled = np.column_stack((np.ones(np.shape(X_train_scaled)[0]), X_train_scaled))
+# X_test_scaled = np.column_stack((np.ones(np.shape(X_test_scaled)[0]), X_test_scaled))
+# Set the first column to 1 since StandardScaler sets it to 0
+X_train_scaled[:, 0] = 1
+X_test_scaled[:, 0] = 1
+# print(X_train_scaled.mean(axis = 0))    # the mean of each feature (column) is 0
+# print(X_train_scaled.std(axis = 0))     # the std of each feature (column) is 1
+# print(X_test_scaled.mean(axis = 0))
+# print(X_test_scaled.std(axis = 0))
 
 
+beta_OLS_train_scaled = OLS(X_train_scaled, z_train)
+ztilde_scaled = X_test_scaled @ beta_OLS_train_scaled
+
+# print(f"MSE train: {MSE(z_train_scaled, ztilde_train_scaled)}")
+print(f"MSE test: {MSE(z_test, ztilde_scaled):.3}")
+print(f"R2Score: {R2Score(z_test, ztilde_scaled):.3}")
 
 # Start figure
 fig = plt.figure()
