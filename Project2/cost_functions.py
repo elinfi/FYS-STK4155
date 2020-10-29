3e4327562160a05488af837266e1fe42cefb3d23
elinfi <elinfins@outlook.com>
2020-10-29 17:35:43 +0100

    Added MSE

---
+++ Project2/cost_functions.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class MSE:
+    def __call__(self, z_tilde, z):
+        return np.mean((z_tilde - z)**2)
+
+    def deriv(self, z_tilde, z):
+        return 2*(z_tilde - z)/z.shape[0]
