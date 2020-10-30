f044f62ad245c56bb8b60042ac670e5572c3e7d0
elinfi <elin.finstad@hotmail.com>
2020-10-30 17:49:12 +0100

    Added new activation functions

--- Project2/activation.py
+++ Project2/activation.py
@@ -1,5 +1,38 @@
 import numpy as np
 
+class Identity:
+    def __call__(self, z):
+        return z
+
+    def deriv(self, z):
+        return np.ones(z.shape)
+
+class LeakyRELU:
+    def __call__(self, z):
+        if z < 0:
+            return 0.01*z
+        else:
+            return z
+
+    def deriv(self, z):
+        if z < 0:
+            return 0.01
+        else:
+            return 1
+
+class RELU:
+    def __call__(self, z):
+        if z > 0:
+            return z
+        else:
+            return 0
+
+    def deriv(self, z):
+        if z > 0:
+            return 1
+        else:
+            return 0
+
 class Sigmoid:
     def __call__(self, z):
         return 1/(1 + np.exp(-z))
@@ -7,9 +40,10 @@ class Sigmoid:
     def deriv(self, z):
         return np.exp(-z)/(1 + np.exp(-z))**2
 
-class Identity:
+class Softmax:
     def __call__(self, z):
-        return z
+        return np.exp(z)/np.sum(np.exp(z), axis=1)[:, None]
 
     def deriv(self, z):
-        return z**0
+        return self.__call__(z) - (self.__call__(z))**2
+        # z * np.ones(len(z)).T.dot(np.eye(len(z)) - np.ones(len(z))*z.T)
