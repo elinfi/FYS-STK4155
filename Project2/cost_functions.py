7916b2730f1ca50affba6a7d1f7e72efdd444bee
elinfi <elin.finstad@hotmail.com>
2020-10-30 17:50:03 +0100

    Added accuracy cost function

--- Project2/cost_functions.py
+++ Project2/cost_functions.py
@@ -6,3 +6,7 @@ class MSE:
 
     def deriv(self, z_tilde, z):
         return 2*(z_tilde - z)/z.shape[0]
+
+class Accuracy:
+    def __call__(self, z_tilde, z):
+        return np.mean(z_tilde == z)
