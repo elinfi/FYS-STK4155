75c3adecaa7b281463a8e3062d428e2e3a9982e3
elinfi <elin.finstad@hotmail.com>
2020-11-03 09:33:08 +0100

    Created Keras class

--- Project2/NN_keras.py
+++ Project2/NN_keras.py
@@ -9,43 +9,24 @@ from tensorflow.keras import optimizers             #This allows using whichever
 from tensorflow.keras import regularizers           #This allows using whichever regularizer we want (l1,l2,l1_l2)
 from tensorflow.keras.utils import to_categorical   #This allows using categorical cross entropy as the cost function
 
+class Keras:
+    def __init__(self, neurons, n_outputs, eta, loss, metrics, input_act, output_act):
+        self.neurons = neurons
+        self.n_outputs = n_outputs
+        self.eta = eta
+        self.loss = loss
+        self.metrics = metrics
+        self.input_act = input_act
+        self.output_act = output_act
 
-epochs = 100
-batch_size = 100
-n_neurons_layer1 = 100
-n_neurons_layer2 = 50
-n_neurons_layer3 = 1
-n = 50
-eta = 0.5
+    def create_neural_network_keras(self):
+        model = Sequential()
 
-def create_neural_network_keras(n_neurons_layer1, n_neurons_layer2, n_categories, eta):
-    model = Sequential()
-    model.add(Dense(n_neurons_layer1, activation='sigmoid'))
-    model.add(Dense(n_neurons_layer2, activation='sigmoid'))
-    # model.add(Dense(n_categories, activation=tf.identity))
-    model.add(Dense(n_categories, activation='softmax'))
+        for i in range(len(self.neurons)):
+            model.add(Dense(self.neurons[i], activation=self.input_act))
+        model.add(Dense(self.n_outputs, activation=self.output_act))
 
-    sgd = optimizers.SGD(lr=eta)
-    model.compile(loss='mean_squared_error', optimizer=sgd)
-    return model
+        sgd = optimizers.SGD(lr=self.eta)
+        model.compile(loss=self.loss, optimizer=sgd, metrics=self.metrics)
 
-if __name__ == '__main__':
-
-    x = np.sort(np.random.uniform(0, 1, n))
-    y = np.sort(np.random.uniform(0, 1, n))
-    x, y = np.meshgrid(x, y)
-    z = np.ravel(f.FrankeFunction(x, y))
-    z = z.reshape(-1, 1)
-
-    data = DataPrep(x, y, z, degree=5)
-    X_train, X_test, z_train, z_test = data()
-
-    DNN = create_neural_network_keras(n_neurons_layer1, n_neurons_layer2, \
-                                      n_outputs, eta=eta)
-    DNN.fit(X_train, z_train, epochs=epochs, batch_size=X_train.shape[0], verbose=0)
-    scores = DNN.evaluate(X_test, z_test)
-
-
-    print("Learning rate = ", eta)
-    print("Test accuracy: %.3f" % scores)
-    print()
+        return model
