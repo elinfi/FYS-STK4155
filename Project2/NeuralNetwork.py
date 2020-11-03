aef5e24494c7048b91bd82a2323261cecdd17400
elinfi <elin.finstad@hotmail.com>
2020-11-03 09:32:45 +0100

    Added function for creating layers

--- Project2/NeuralNetwork.py
+++ Project2/NeuralNetwork.py
@@ -19,9 +19,18 @@ class DenseLayer:
         return self.a
 
 class NeuralNetwork:
-    def __init__(self, layers, cost):
-        self.layers = layers
+    def __init__(self, n_inputs, neurons, n_outputs, cost):
+        self.n_inputs = n_inputs
+        self.neurons = neurons
+        self.n_outputs = n_outputs
         self.cost = cost
+
+    def create_layers(self, activation, output_activation):
+        self.layers = []
+        self.layers.append(DenseLayer(self.n_inputs, self.neurons[0], activation)) # input layer
+        for i in range(len(self.neurons) - 1):
+            self.layers.append(DenseLayer(self.neurons[i], self.neurons[i + 1], activation)) # hidden layers
+        self.layers.append(DenseLayer(self.neurons[-1], self.n_outputs, output_activation)) # output layer
 
     def feedforward(self, a):
         for layer in self.layers:
@@ -43,18 +52,21 @@ class NeuralNetwork:
 
         self.feedforward(X)
 
-x = np.linspace(0,1,100).reshape(-1,1)
-y = x**2 + 0.1*np.random.randn(100, 1)
+if __name__ == '__main__':
 
-layer1 = DenseLayer(1, 10, act.Sigmoid())
-layer2 = DenseLayer(10, 10, act.Sigmoid())
-layer3 = DenseLayer(10, 1, act.Identity())
 
-layers = [layer1, layer2, layer3]
-network = NeuralNetwork(layers, cost.MSE())
-network.feedforward(x)
-print(f.MSE(y, layer3.a))
+    x = np.linspace(0,1,100).reshape(-1,1)
+    y = x**2 + 0.1*np.random.randn(100, 1)
 
-for i in range(1000):
-    network.backprop(x, y, 0.5)
-print(f.MSE(y, layer3.a))
+    layer1 = DenseLayer(1, 10, act.Sigmoid())
+    layer2 = DenseLayer(10, 10, act.Sigmoid())
+    layer3 = DenseLayer(10, 1, act.Identity())
+
+    layers = [layer1, layer2, layer3]
+    network = NeuralNetwork(layers, cost.MSE())
+    network.feedforward(x)
+    print(f.MSE(y, layer3.a))
+
+    for i in range(1000):
+        network.backprop(x, y, 0.5)
+    print(f.MSE(y, layer3.a))
