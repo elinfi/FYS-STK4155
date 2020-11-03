af6242bf0046565664337684d79261764f385893
elinfi <elin.finstad@hotmail.com>
2020-11-03 09:34:28 +0100

    Updated call to classes

--- Project2/NN_vs_keras.py
+++ Project2/NN_vs_keras.py
@@ -5,12 +5,9 @@ import cost_functions as cost
 import matplotlib.pyplot as plt
 from data_prep import DataPrep
 from NeuralNetwork import DenseLayer, NeuralNetwork
-from NN_keras import create_neural_network_keras
+from NN_keras import Keras
 
-n = 100
-n_epochs = 100
-eta = 0.5
-
+n = 20
 x = np.sort(np.random.uniform(0, 1, n))
 y = np.sort(np.random.uniform(0, 1, n))
 x, y = np.meshgrid(x, y)
@@ -18,36 +15,37 @@ z = np.ravel(f.FrankeFunction(x, y))
 z = z.reshape(-1, 1)
 
 data = DataPrep()
-X = data.design_matrix(x, y, degree=1)
-X_train, X_test, z_train, z_test = data.train_test_scale(X, z)
+X = data.design_matrix(x, y, degree=5)
+X_train, X_test, z_train, z_test = data.train_test_split(X, z)
 X_train, X_test,= X_train[:, 1:], X_test[:, 1:]
 
+
 n_inputs = X_train.shape[1]
-n_neurons_layer1 = 10
-n_neurons_layer2 = 6
+neurons = [50, 20, 100, 50, 50, 20]
 n_outputs = 1
 
-layer1 = DenseLayer(n_inputs, n_neurons_layer1, act.Sigmoid())
-layer2 = DenseLayer(n_neurons_layer1, n_neurons_layer2, act.Sigmoid())
-layer3 = DenseLayer(n_neurons_layer2, n_outputs, act.Softmax())
-
-layers = [layer1, layer2, layer3]
-network = NeuralNetwork(layers, cost.MSE())
+network = NeuralNetwork(n_inputs, neurons, n_outputs, cost.MSE())
+network.create_layers(act.Sigmoid(), act.Softmax())
 network.feedforward(X_train)
-print(f.MSE(z_train, layer3.a))
+print(f.MSE(z_train, network.layers[-1].a))
 
+
+n_epochs = 500
+eta = 0.5
 for i in range(n_epochs):
     network.backprop(X_train, z_train, eta)
 
-print(f.MSE(z_train, layer3.a))
-
+print(f"MSE train {f.MSE(z_train, network.layers[-1].a)}")
 network.feedforward(X_test)
-print(f"her {f.MSE(z_test, layer3.a)}")
+print(f"MSE test {f.MSE(z_test, network.layers[-1].a)}")
 
-DNN = create_neural_network_keras(n_neurons_layer1, n_neurons_layer2,\
-                                  n_outputs, eta=eta)
-DNN.fit(X_train, z_train, epochs=n_epochs, batch_size=X_train.shape[0], verbose=0)
-scores = DNN.evaluate(X_test, z_test)
+
+NN_keras = Keras(neurons, n_outputs, eta, loss='mean_squared_error', \
+                 metrics=[], input_act='sigmoid', output_act='softmax')
+model = NN_keras.create_neural_network_keras()
+model.fit(X_train, z_train, epochs=n_epochs, batch_size=X_train.shape[0], verbose=0)
+scores = model.evaluate(X_test, z_test)
+scores = model.evaluate(X_train, z_train)
 
 
 print("Learning rate = ", eta)
