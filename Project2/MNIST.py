8d8dbf1f5dd18eabd5d7769fe9392ee85b36b6e4
elinfi <elin.finstad@hotmail.com>
2020-11-04 13:56:43 +0100

    Small changes

--- Project2/MNIST.py
+++ Project2/MNIST.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+from sklearn import datasets
+
 import activation as act
 import cost_functions as cost
-from sklearn import datasets
 from data_prep import DataPrep
 from NeuralNetwork import NeuralNetwork
 from NN_keras import Keras
@@ -31,33 +33,36 @@ n_outputs = 10
 
 network = NeuralNetwork(n_inputs, neurons, n_outputs, cost.MSE())
 network.create_layers(act.Sigmoid(), act.Softmax())
-network.feedforward(X_train)
 
 n_epochs = 500
-eta = 0.5
+eta = 0.1
 for i in range(n_epochs):
     network.backprop(X_train, z_train, eta)
 
 network.feedforward(X_test)
 
 accuracy = cost.Accuracy()
-z_tilde = (network.layers[-1].a)
+# z_tilde = np.argmax(network.layers[-1].a, axis=1)
+z_tilde = network.layers[-1].a
 print(z_tilde)
-print(z_test)
+print(np.argmax(z_test, axis=1))
 print(accuracy(z_tilde, np.argmax(z_test, axis=1)))
 
-NN_keras = Keras(neurons, n_outputs, eta, loss='mean_squared_error', \
-                 metrics=['accuracy'], input_act='sigmoid', output_act='softmax')
-model = NN_keras.create_neural_network_keras()
-model.fit(X_train, z_train, epochs=n_epochs, batch_size=X_train.shape[0], verbose=0)
-scores = model.evaluate(X_test, z_test)
-scores = model.evaluate(X_train, z_train)
+
+# NN_keras = Keras(neurons, n_outputs, eta, loss='categorical_crossentropy',
+#                  metrics=['accuracy'], input_act='relu',
+#                  output_act='softmax')
+# model = NN_keras.create_neural_network_keras()
+# model.fit(X_train, z_train, epochs=n_epochs, batch_size=X_train.shape[0])
+# scores = model.evaluate(X_test, z_test)
+# scores = model.evaluate(X_train, z_train)
 
 
-print('HEIHEIHEI')
-print(model.predict(X_test))
-z_tilde = np.argmax(model.predict(X_train), axis=1).reshape(-1, 1)
-print(z_tilde)
-print(z_train - z_tilde)
-print(z_train.shape)
-print(accuracy(z_tilde, np.argmax(z_train)))
+
+# print(np.argmax(model.predict(X_test), axis=1))
+# print(np.argmax(z_test, axis=1))
+# z_tilde = np.argmax(model.predict(X_test), axis=1)
+# print(z_tilde.shape)
+# # print(z_test - z_tilde)
+# print(z_train.shape)
+# print(accuracy(z_tilde, np.argmax(z_test, axis=1)))
