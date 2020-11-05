ab822b0f518b4c4759808e58470ba5e44625f555
elinfi <elin.finstad@hotmail.com>
2020-11-05 16:04:48 +0100

    Small changes

--- Project2/MNIST.py
+++ Project2/MNIST.py
@@ -6,46 +6,46 @@ from sklearn import datasets
 import activation as act
 import cost_functions as cost
 from data_prep import DataPrep
-from NeuralNetwork import NeuralNetwork
+from NeuralNetwork import NeuralNetwork, DenseLayer
 from NN_keras import Keras
 
 # download MNIST dataset
 digits = datasets.load_digits()
 
 # define inputs and labels
-inputs = digits.images
+dataset = digits.images
 labels = digits.target.reshape(-1, 1)
 
 # flatten the image
-# the value -1 means dimension is inferred from the remaining dimensions: 8x8 = 64
-n_inputs = len(inputs)
-inputs = inputs.reshape(n_inputs, -1)
-print("X = (n_inputs, n_features) = " + str(inputs.shape))
+N = len(dataset)
+dataset = dataset.reshape(N, -1)
 
-
+# Transform labels to onehot vectors and split in train and test
 data = DataPrep()
-one_hot = data.create_one_hot(n_inputs, labels)
-X_train, X_test, z_train, z_test = data.train_test_split(inputs, one_hot)
+one_hot = data.create_one_hot(N, labels)
+X_train, X_test, z_train, z_test = data.train_test_split(dataset, one_hot)
 
 n_inputs = X_train.shape[1]
-neurons = [50, 20, 100, 50, 20]
+neurons = []
 n_outputs = 10
 
 network = NeuralNetwork(n_inputs, neurons, n_outputs, cost.MSE())
-network.create_layers(act.Sigmoid(), act.Softmax())
+network.create_layers(act.Sigmoid(), act.Sigmoid())
+
 
 n_epochs = 500
-eta = 0.1
+eta = 0.001
+lmbda = 0
 for i in range(n_epochs):
-    network.backprop(X_train, z_train, eta)
+    network.backprop_two_layers(X_train, z_train, eta, lmbda)
 
 network.feedforward(X_test)
 
 accuracy = cost.Accuracy()
 # z_tilde = np.argmax(network.layers[-1].a, axis=1)
-z_tilde = network.layers[-1].a
+z_tilde = np.argmax(network.layers[-1].a, axis=1)
+# print(np.argmax(z_test, axis=1))
 print(z_tilde)
-print(np.argmax(z_test, axis=1))
 print(accuracy(z_tilde, np.argmax(z_test, axis=1)))
 
 
