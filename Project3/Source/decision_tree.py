f1270587c7a311255c6ae69072810c7e13c7a6c4
elinfi <elin.finstad@hotmail.com>
2020-12-07 10:41:51 +0100

    Added comments

--- Project3/Source/decision_tree.py
+++ Project3/Source/decision_tree.py
@@ -3,34 +3,37 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 from preprocessing import preprocessing
 
-filename = 'data_trim.csv'
+# preprocessing of data
+filename = '../Data/data_trim.csv'
 data = preprocessing(filename)
 corpus = data['tweet']
 labels = data['label']
 
-vectorizer = CountVectorizer(min_df=5, max_df=0.99)
+# create bag of words
+vectorizer = TfidfVectorizer(min_df=5, max_df=0.99)
 bow = vectorizer.fit_transform(corpus)
+# print(bow)
+print(bow.shape)
 
-bow_train, bow_test, labels_train, labels_test = train_test_split(bow, labels, test_size=0.3)
+# split in train and test data
+bow_train, bow_test, labels_train, labels_test = train_test_split(bow, labels,
+                                                                  test_size=0.3)
 
-print(bow_test.shape)
-print(bow_train.shape)
-
-dct = DecisionTreeClassifier(criterion='gini', max_depth=40)
+# create decision tree
+dct = DecisionTreeClassifier(criterion='gini', max_depth=500)
 dct.fit(bow_train, labels_train)
 
-dct_bow_test = dct.predict(bow_test)
-dct_bow_train = dct.predict(bow_train)
+# predict sentiment of tweets
+dct_pred_test = dct.predict(bow_test)
+dct_pred_train = dct.predict(bow_train)
 
-print(dct_bow_test.shape)
-print(dct_bow_test)
-print(dct_bow_train)
-print(accuracy_score(labels_test, dct_bow_test))
-print(accuracy_score(labels_train, dct_bow_train))
+# calculate accuracy score
+print(accuracy_score(labels_test, dct_pred_test))
+print(accuracy_score(labels_train, dct_pred_train))
 # df_bow = pd.DataFrame(bow.todense())
 
 # print(df_bow)
